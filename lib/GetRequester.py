cf56c19ad47f3fe04d219634ae62bda9576bac1f
Doris <doris.kiprop@student.moringaschool.com>
2024-03-18 00:03:55 +0300

    ok

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,3 +1,17 @@
+# import requests
+# import json
+
+# class GetRequester:
+
+#     def __init__(self, url):
+#         self.url = url
+
+#     def get_response_body(self):
+#         pass
+
+#     def load_json(self):
+#         pass
+
 import requests
 import json
 
@@ -7,7 +21,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response = json.loads(self.get_response_body())
+        return response
+
+url = " https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+test = GetRequester(url)
+print(test.load_json())
